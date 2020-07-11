79e7e76b922dd2a51efe95b6e0027b6beed717f1
Aritynn <66809567+Aritynn@users.noreply.github.com>
2020-07-11 03:25:31 -0500

    Removes "Complete name" line from MediaInfo

--- 8-DVD-create-thumbnails-(drag-VIDEOS_TS-from-disc-onto-bat-file).bat
+++ 8-DVD-create-thumbnails-(drag-VIDEOS_TS-from-disc-onto-bat-file).bat
@@ -101,7 +101,8 @@ IF %enableIFOMediaInfo%==true (
         )
     )
 
-    mediainfo.exe "!ifoFile!">"%outputDirectory%%infoDirectory%IFO-mediainfo.txt"
+    mediainfo.exe "!ifoFile!">%tempFile%
+    TYPE %tempFile% | FINDSTR /V /B /L /C:"Complete name" >>"%outputDirectory%%infoDirectory%IFO-mediainfo.txt"
 
     FOR /F "usebackq tokens=1,2 delims==" %%i in (`WMIC OS GET LocalDateTime /VALUE 2^>NUL`) DO IF '.%%i.'=='.LocalDateTime.' SET ldt=%%j
     SET currentTime=!ldt:~0,4!-!ldt:~4,2!-!ldt:~6,2!-!ldt:~8,2!h!ldt:~10,2!m!ldt:~12,2!s
@@ -298,7 +299,8 @@ REM %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Get VOB Info %%%%%%
 
 IF %enableVOBMediaInfo%==true (
     ECHO [SPOILER=VOB MediaInfo]>"%outputDirectory%%infoDirectory%VOB-description.txt"
-    mediainfo.exe "%transferredFolderPath%%driveLabel%\VIDEO_TS\VTS_0!EpisodeFound!_1.VOB">>"%outputDirectory%%infoDirectory%VOB-description.txt"
+    mediainfo.exe "%transferredFolderPath%VIDEO_TS\VTS_0!EpisodeFound!_1.VOB" >%tempFile%
+    TYPE %tempFile% | FINDSTR /V /B /L /C:"Complete name" >>"%outputDirectory%%infoDirectory%VOB-description.txt"
     ECHO [/SPOILER]>>"%outputDirectory%%infoDirectory%VOB-description.txt"
     ECHO. >>"%outputDirectory%%infoDirectory%VOB-description.txt"
     ECHO [CENTER]>>"%outputDirectory%%infoDirectory%VOB-description.txt"
